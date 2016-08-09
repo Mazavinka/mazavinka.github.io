d6f480ef19392a99611a0110fa75ce7ad7b66687
Mazavinka <Mazavinka2010@gmail.com>
2016-08-10 01:21:37 +0400

    commit

--- js/jq.js
+++ js/jq.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
     $('#go').click(function () {
         var zameska = $("input[name=zameska]").val();
         var navesah = $("input[name=navesah]").val();
-        var ostatok = ((zameska - navesah) / zameska).toPrecision(2);
+        var ostatok = ((zameska - navesah) / zameska).toPrecision(3);
         var lateks = ostatok * 2900;
-        var mel = ostatok * 5100; 
+        var mel = ostatok * 5100;
+		var water = ostatok * 300;
         $('#lat').empty();
         $('#mel').empty();
+		$('#water').empty();
         $('#lat').append("Need Latex: " + lateks);
         $('#mel').append("Need Mel: " + mel);
+		$('#water').append("Need Water: " + water);
     });
 });
